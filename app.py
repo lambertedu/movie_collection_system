b8d470aeb93a1385be68244b4842ced81adc275a
Lambert Kakraba <lambertedudzi95@gmail.com>
2025-07-20 00:25:44 +0000

    initial commit for movie collection system

---
+++ app.py
@@ -0,0 +1,55 @@
+MENU_PROMPT = "Eenter 'a' to add a movie, 'l' to list movies, 'f' to find a movie, 'q' to quit: "
+movies = []
+
+
+def add_movie():
+    title = input("Enter movie title: ")
+    director = input("Enter movie director: ")
+    year = input("Enter movie relaese year: ")
+
+    movies.append({
+        "title": title,
+        "director": director,
+        "year": year
+    })
+    print("Movies added successfully!\n")
+
+
+def list_movie():
+    for movie in movies:
+        print_movie(movie)
+
+
+def find_movie():
+    search_movie = input("Enter movie title to search: ")
+
+    for movie in movies:
+        if search_movie.lower() == movie['title'].lower():
+            print("movie found!")
+            print_movie(movie)
+            return
+    print("Movie not found")
+
+
+def print_movie(movie):
+    print(
+        f"Title: {movie['title']}\nDirector: {movie['director']}\nYear: {movie['year']}\n")
+
+
+def menu():
+    selection = input(MENU_PROMPT)
+
+    while selection != 'q':
+        if selection == 'a':
+            add_movie()
+        elif selection == 'l':
+            list_movie()
+
+        elif selection == 'f':
+            find_movie()
+        else:
+            print("Invalid selection. Try again\n")
+        selection = input(MENU_PROMPT)
+
+
+menu()
